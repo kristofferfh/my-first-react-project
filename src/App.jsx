f04ba195f897d0393168dc8d09b037e8ba4c5bee
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-06-20 13:09:30 +0200

    attempted route fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,6 @@ import Blog from './routes/blog'
 import Contact from './routes/contact'
 import Projects from './routes/projects'
 
-/* check https://reactrouter.com/en/main/components/outlet
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Navbar />}>
-          <Route index element={<LandingPage />} />
-          <Route path='projects' element={<h1>my projects</h1>} />
-          <Route path='contact' element={<h1>contact me</h1>} />
-          <Route path='*' element={<h1>Four ohh four</h1>} />            
-        </Route>
-      </Routes>
-    </Router>    
-  )
-} */
-
 export default function App() {
   return (
     <>
@@ -30,12 +14,12 @@ export default function App() {
       <Router>
         <Routes>
           <Route index element={<Blog />} />
-          <Route path='article'>
+          {/* <Route path='article'>
             <Route path=':articleSlug' element={<p>test</p>} />
-          </Route>
+          </Route> */}
           <Route path='projects' element={<Projects />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<h1 >Four 😫hh four</h1>} />
+          {/* <Route path='*' element={<h1 >Four 😫hh four</h1>} /> */}
         </Routes>
       </Router>    
     </>
